252fe121c81258a66431ff0f64fd63a0ff7866ad
ziyama <heronolimit1@gmail.com>
2026-03-18 06:23:51 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { AptosWalletAdapterProvider } from '@aptos-labs/wallet-adapter-react';
 import App from './App';
 import './index.css';
 
+function Root() {
+  return (
+    <AptosWalletAdapterProvider autoConnect={false} optInWallets={['Petra']}>
+      <App />
+    </AptosWalletAdapterProvider>
+  );
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>
 );
